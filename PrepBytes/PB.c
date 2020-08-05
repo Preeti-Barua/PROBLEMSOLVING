11d975a73bc9cdbad06f39d99ee51fc425924dba
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-08-05 17:05:35 +0530

    Boxes & toys

---
+++ PrepBytes/PB.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	           int i,t,c,N;
+	           int a;
+                         int count=0;
+                         scanf("%d",&N);
+                         while(N--)
+                         {   
+                         
+                           
+                           scanf("%d",&t);
+                           scanf("%d",&c);
+                           
+                            a=c-t;
+                          
+                           if(c-t>=2)
+                           {
+                             count++;
+                           }
+                           
+                          
+                           
+                           
+                           
+                         }
+                         printf("%d", count);
+  
+  return 0;
+}
+                         	
+                         	
+                         	
+                         	
+                         
+                         
+                         
+	
+	
+	
+	
+
