c56d50d2c95d684d1e7495e74e2a7735e685c8be
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-08-08 23:35:35 +0530

    socks

---
+++ PrepBytes/HR9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	                int n,p;
+	                scanf("%d",&n);
+	                scanf("%d",&p);
+	                
+	                
+	                
+	                int totalpage=n/2;
+	                int targetpage=p/2;
+	                int ans= totalpage - targetpage;
+	                printf("%d",ans);
+	                
+	                
+}
