160ddcff569129693dd727b85e7a35cccdd9e50d
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2020-08-20 23:14:30 +0530

    snippets

---
+++ practice1.c
@@ -0,0 +1,23 @@
+  /*
+    #include <stdio.h>
+    int main()
+    {
+        int a = 0, i = 0, b;
+        for (i = 0;i < 5; i++)
+        {
+            a++;
+            if (i == 3)
+                break;
+        }
+    }
+    */
+    
+    
+    #include<stdio.h>
+main()
+{
+   // typedef int a;
+    int a ,b=2, c=8, d;
+    d=(b*2)/2+8;
+    printf("%d",d);
+}
