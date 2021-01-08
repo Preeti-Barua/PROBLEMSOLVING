9997ddf08397cf512dfed3ff90818b03ab8e1010
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2021-01-08 22:23:35 +0530

    pointers

---
+++ pointers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+//	         int n=10;
+//	         int *p;
+//	         p=&n;
+//	         
+//	         printf("%d\n" ,n);
+//	         printf("%d\n",*p);
+//	         printf("%d\n",&n);
+//	         printf("%d\n",p);
+//	         
+	         
+	         int A[]={13,643,218,34,2,7,654,8};
+	         int *p=A;
+	         int i,a;
+	         for( i=1;i<7;i++)
+	         {
+			 
+	         printf("%d\n",*(A+i));
+	     }
+	         
+}
