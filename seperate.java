c7a970783008427ba8e6f0292ff3f06bcc5c051a
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2021-01-18 22:25:45 +0530

    basics

---
+++ seperate.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class seperate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+//		Scanner sc= new Scanner(System.in);
+//		String s=sc.nextLine();
+//		
+//		
+//		for(int i=0;i<s.length();i++)
+//		{
+//			System.out.print(s.charAt(i)+" ");
+//		}
+		
+		
+		
+		
+//         String s="swastika";
+
+//         int i,j;
+//         for(i=s.length()-1;i>=0;i--)
+//        	 
+//         {
+//        	 
+//        	 System.out.print(s.charAt(i)+ " ");
+//         }
+//         
+//		
+		
+
+//		 String s="swastika";
+//		 int count=0;
+//		 for(int i=0;i<s.length();i++)
+//		 {
+//			 count++;
+//		 }
+//		 System.out.println(count);
+//			 
+		
+		
+		
+		
+//		 String s="swastika";
+//		
+//		 for(int i=0;i<s.length()-1;i++)
+//		 {
+//			 for(int j=i+1;j<s.length();j++)
+//			 {
+//			 if(s.charAt(i)==s.charAt(j))
+//			 {
+//				 System.out.println(s.charAt(i));
+//			 }
+//			 
+//			
+//			 }
+//		 }
+		
+		
+		
+	    String s="swastika";
+	    String sb="st";
+	  
+	    
+	    for(int i=0;i<s.length()-1;i++)
+	    {
+	    	
+	    	if(s.charAt(i)==sb.charAt(i))
+	    	{
+	    		System.out.println("Present");
+	    	}
+	    	else
+	    	{
+	    		System.out.println("Not Present");
+	    	}
+	    	
+	    	
+	    }
+	   
+		
+		
+		
+	     
+		
+		
+
+	}
+}
+
+
