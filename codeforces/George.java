99e5b71081408d841f48c73a62f858167fd1fa3f
PREETI <40754414+Preeti-Barua@users.noreply.github.com>
2021-01-26 20:51:42 +0530

    george and Accommodation
    https://codeforces.com/problemset/problem/467/A

---
+++ codeforces/George.java
@@ -0,0 +1,39 @@
+package codeforces;
+//https://codeforces.com/problemset/problem/467/A
+import java.util.Scanner;
+
+public class George {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+	    int a = 0,p=0;
+	    int count=0;
+		int f=sc.nextInt();
+		for(int i=0;i<f;i++)
+		{
+			 a=sc.nextInt();
+			 p=sc.nextInt();
+			
+			    
+			    if(a-p>=2)
+				{
+					count++;
+					
+				}
+			    
+		
+		}
+		System.out.println(count);
+		}
+		}
+	
+		
+		
+		
+	
+	
+	
+		
+   
+
