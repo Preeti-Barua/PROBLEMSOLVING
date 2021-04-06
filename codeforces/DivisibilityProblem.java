6f16be6551c23e17671798afdf2ba77291b4c2bd
Preeti-Barua <Preetibarua98@gmail.com>
2021-04-06 23:01:21 +0530

    commiting with other logic

--- codeforces/DivisibilityProblem.java
+++ codeforces/DivisibilityProblem.java
@@ -1,29 +1,32 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class DivisibilityProblem {
+public class DivisibilityProblem {
 
-// 	public static void main(String[] args)
-// 	{
-// 		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
 
-// 		Scanner sc=new Scanner(System.in);
-// 		int t=sc.nextInt();
-// 		for(int i=0;i<t;i++)
-// 		{
-// 			int a=sc.nextInt();
-// 			int b=sc.nextInt();
-// 			int count=0;
-// 			while(a%b!=0)
-// 			{
-// 				a++;
-// 				count++;
-// 			}
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+			int a=sc.nextInt();
+				int b=sc.nextInt();
+ 
+ 
+				if(a%b==0)
+				{
+					System.out.println(0);
+				}
+				else
+				{
+					int l=a%b;
+					System.out.println(b-l);
+				}
 			
-// 			System.out.println(count);
 			
 			
-// 		}
-// 	}
+		}
+	}
 
-// }
-
+}
